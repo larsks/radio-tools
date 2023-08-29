e854608d61a811d34419b3c8bcf7f6a58c4e77b1
Lars Kellogg-Stedman <lars@oddbit.com>
2023-08-28 22:54:10 -0400

    Adjust arp timing for ax25

--- axup.sh
+++ axup.sh
@@ -9,6 +9,8 @@ if [[ -z $1 ]]; then
 	exit 1
 fi
 
+set -e
+
 kissattach $(readlink /tmp/kisstnc) $1 $2
 kissparms -p $1 -c 1 \
 	${FULLDUPLEX:+-f ${FULLDUPLEX}} \
@@ -16,3 +18,8 @@ kissparms -p $1 -c 1 \
 	${TXTAIL:+-l ${TXTAIL}} \
 	${PERSIST:+-r ${PERSIST}} \
 	${SLOTTIME:+-s ${SLOTTIME}}
+
+# https://thomask.sdf.org/blog/2017/08/06/improving-arp-performance-on-slow-ax-25-links.html
+sysctl net.ipv4.neigh.ax0.retrans_time_ms=5000
+sysctl net.ipv4.neigh.ax0.base_reachable_time_ms=1200000
+
