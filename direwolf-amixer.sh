23d34d11ea443298fb7960aa74f4c8fb7bf65827
Lars Kellogg-Stedman <lars@oddbit.com>
2023-10-06 22:30:36 -0400

    Default mixer values to 0%

--- direwolf-amixer.sh
+++ direwolf-amixer.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 : "${DIREWOLF_CONFIG:=${XDG_CONFIG_HOME:-$HOME/.config}/direwolf/direwolf.conf}"
-: "${ALSA_SPEAKER_LEVEL:=80%}"
-: "${ALSA_CAPTURE_LEVEL:=40%}"
+: "${ALSA_SPEAKER_LEVEL:=0%}"
+: "${ALSA_MIC_LEVEL:=0%}"
+: "${ALSA_CAPTURE_LEVEL:=0%}"
 
 ADEVICE=$(awk '$1 == "ADEVICE" {print $2}' $DIREWOLF_CONFIG)
 
 echo "Setting sound levels on $ADEVICE"
 amixer -D "${ADEVICE}" -s <<EOF
 cset name='Speaker Playback Volume' $ALSA_SPEAKER_LEVEL
+cset name='Mic Playback Volume' $ALSA_MIC_LEVEL
 cset name='Mic Capture Volume' $ALSA_CAPTURE_LEVEL
 cset name='Auto Gain Control' off
 EOF
