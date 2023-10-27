dd3d1060f6dc15bf93b29444f9eac2660977f215
Lars Kellogg-Stedman <lars@oddbit.com>
2023-10-26 23:18:46 -0400

    Adopt ax25 config from pat

--- roles/ax25/files/post-up.sh
+++ roles/ax25/files/post-up.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-FULLDUPLEX=y
+: "${AX25_HBAUD:=1200}"
 
-set -e
-
-kissparms -p $AX25_PORT -c 1 \
-	${AX25_FULLDUPLEX:+-f ${AX25_FULLDUPLEX}} \
-	${AX25_TXDELAY:+-t ${AX25_TXDELAY}} \
-	${AX25_TXTAIL:+-l ${AX25_TXTAIL}} \
-	${AX25_PERSIST:+-r ${AX25_PERSIST}} \
-	${AX25_SLOTTIME:+-s ${AX25_SLOTTIME}}
+if [ "$AX25_HBAUD" -eq "9600" ]; then
+  /usr/sbin/kissparms -p $AX25_PORT -c 1 -t 100 -l 10 -s 12 -r 80 -f n
+  echo 4      > /proc/sys/net/ax25/ax0/standard_window_size  # 2-7 (max frames)
+  echo 256    > /proc/sys/net/ax25/ax0/maximum_packet_length # 1-512 (paclen)
+  echo 3100   > /proc/sys/net/ax25/ax0/t1_timeout            # (Frack /1000 = seconds)
+  echo 800    > /proc/sys/net/ax25/ax0/t2_timeout            # (RESPtime /1000 = seconds)
+  echo 300000 > /proc/sys/net/ax25/ax0/t3_timeout            # (Check /1000 = seconds)
+  echo 100000  > /proc/sys/net/ax25/ax0/idle_timeout         # (/10000(?) = seconds)
+  echo 5      > /proc/sys/net/ax25/ax0/maximum_retry_count   # n
+  echo 2      > /proc/sys/net/ax25/ax0/connect_mode          # 0 = None, 1 = Network, 2 = All
+elif [ "$AX25_HBAUD" -eq "1200" ]; then
+  /usr/sbin/kissparms -p $AX25_PORT -c 1 -t 300 -l 10 -s 12 -r 80 -f n
+  echo 4      > /proc/sys/net/ax25/ax0/standard_window_size
+  echo 128    > /proc/sys/net/ax25/ax0/maximum_packet_length
+  echo 2000   > /proc/sys/net/ax25/ax0/t1_timeout
+  echo 1000   > /proc/sys/net/ax25/ax0/t2_timeout
+  echo 300000 > /proc/sys/net/ax25/ax0/t3_timeout
+  echo 100000   > /proc/sys/net/ax25/ax0/idle_timeout
+  echo 5      > /proc/sys/net/ax25/ax0/maximum_retry_count
+  echo 2      > /proc/sys/net/ax25/ax0/connect_mode
+else
+  echo "Invalid HBAUD $3"
+  return 1;
+fi
 
 # https://thomask.sdf.org/blog/2017/08/06/improving-arp-performance-on-slow-ax-25-links.html
 sysctl net.ipv4.neigh.ax0.retrans_time_ms=5000
