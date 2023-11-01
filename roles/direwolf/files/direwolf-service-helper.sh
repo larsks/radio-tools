da7d06c27e811dcb47c9adcfeabf70012c5928de
Lars Kellogg-Stedman <lars@oddbit.com>
2023-11-01 13:42:16 -0400

    Allow arbitrary options in DIREWOLF_OPTIONS env var/

--- roles/direwolf/files/direwolf-service-helper.sh
+++ roles/direwolf/files/direwolf-service-helper.sh
@@ -3,7 +3,6 @@
 DEFAULT_DIREWOLF_CONFIG="/etc/radio/direwolf.conf"
 
 : "${DIREWOLF:=direwolf}"
-: "${DIREWOLF_FX25:=1}"
 : "${DIREWOLF_CONFIG:=$DEFAULT_DIREWOLF_CONFIG}"
 
 : "${ALSA_SPEAKER_LEVEL:=0%}"
@@ -23,6 +22,6 @@ cset name='Auto Gain Control' off
 EOF
 
 exec ${DIREWOLF} -t 0 \
-	-X ${DIREWOLF_FX25} \
 	${DIREWOLF_KISSTNC:+-p} \
-	-c ${DIREWOLF_CONFIG}
+	-c ${DIREWOLF_CONFIG} \
+	${DIREWOLF_OPTIONS}
