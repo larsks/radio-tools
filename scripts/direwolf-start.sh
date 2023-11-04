3d146bba55accdd694374cdb1c264eee34309298
Lars Kellogg-Stedman <lars@oddbit.com>
2023-11-04 00:35:12 -0400

    Fix shell errors in direwolf-start.sh

--- scripts/direwolf-start.sh
+++ scripts/direwolf-start.sh
@@ -5,19 +5,19 @@ set -e
 DIREWOLF_ARGS=()
 
 if [[ -n $DIREWOLF_FX25 ]]; then
-	DIREWOLF_ARGS+=(-X 1)
+	DIREWOLF_ARGS+=( -X 1 )
 fi
 
 if [[ -n $DIREWOLF_IL2P ]]; then
 	if [[ -n $DIREWOLF_IL2P_INV ]]; then
-		DIREWOLF_ARGS+=(-i 1)
+		DIREWOLF_ARGS+=( -i 1 )
 	else
-		DIREWOLF_ARGS+=(-I 1)
+		DIREWOLF_ARGS+=( -I 1 )
 	fi
 fi
 
 if [[ -n $DIREWOLF_KISSPTY ]]; then
-	DIREWOLF_ARGS+="-p"
+	DIREWOLF_ARGS+=( -p )
 fi
 
 exec direwolf -t 0 -q d -c /etc/radio/direwolf.conf "${DIREWOLF_ARGS[@]}"
