bf51bdea7e6329b311397faa453785b5a6b5550d
Lars Kellogg-Stedman <lars@oddbit.com>
2023-11-04 00:37:54 -0400

    Fix variable names in ax25tnc-start.sh

--- scripts/ax25tnc-start.sh
+++ scripts/ax25tnc-start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
 ax25_tty_resolved=$(readlink -f "$AX25_TTY")
-ax25_port=$2
-ax25_speed=$3
 
-  /usr/sbin/kissattach "${ax25_tty}" "${ax25_port}" -m 256
+/usr/sbin/kissattach "${ax25_tty_resolved}" "${AX25_PORT}" -m 256
