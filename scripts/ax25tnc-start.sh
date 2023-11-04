f820b2243a8b6284d4c5bf4b9cd7f7fce1c07a4b
Lars Kellogg-Stedman <lars@oddbit.com>
2023-11-04 00:43:44 -0400

    Reduce MTU on vhf0

--- scripts/ax25tnc-start.sh
+++ scripts/ax25tnc-start.sh
@@ -2,4 +2,4 @@
 
 ax25_tty_resolved=$(readlink -f "$AX25_TTY")
 
-/usr/sbin/kissattach "${ax25_tty_resolved}" "${AX25_PORT}" -m 256
+/usr/sbin/kissattach "${ax25_tty_resolved}" "${AX25_PORT}" -m ${AX25_MTU:-128}
